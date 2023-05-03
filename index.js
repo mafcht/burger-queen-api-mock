2540c150f3b5225d92dfcc8a070952050adb5761
mafcht <mfernanda.cht@gmail.com>
2023-05-03 16:03:34 -0600

    test3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express ();
+// const express = require("express");
+// const app = express ();
 
-const port = process.env.PORT || 400;
+// const port = process.env.PORT || 400;
 
-app.listen(port);
+// app.listen(port);
 
-app.get("/", (req,res) => {
-    res.send("Initial page")
-});
+// app.get("/", (req,res) => {
+//     res.send("Initial page")
+// });
 
-console.log(`listen on port ${port}`);
+// console.log(`listen on port ${port}`);
