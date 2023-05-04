6066e5718564ed8bfcc02fc6a2a98717cbbebdcc
mafcht <mfernanda.cht@gmail.com>
2023-05-04 13:13:44 -0600

    added link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 'https://burger-queen-api-mock-nib4.vercel.app/';
 
 const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
 
