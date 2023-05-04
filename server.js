ba7284fc081251508dffaeea73a3c4e08781c369
mafcht <mfernanda.cht@gmail.com>
2023-05-04 14:55:00 -0600

    test2

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const middlewares = jsonServer.defaults()
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
+const tmpdir = process.env.TMPDIR || 'https://burger-queen-api-mock-nib4.vercel.app/tmp'; // Obtener la ruta de la carpeta temporal o utilizar una ruta predeterminada como /tmp
+
+
 // const port = process.env.PORT || 'https://burger-queen-api-mock-nib4.vercel.app/';
 
 const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
@@ -23,9 +26,3 @@ app.listen(port, () => {
     console.log(`JSON Server is running in ${port}`);
 });
 
-const tmpdir = process.env.TMPDIR || 'https://burger-queen-api-mock-nib4.vercel.app/tmp'; // Obtener la ruta de la carpeta temporal o utilizar una ruta predeterminada como /tmp
-
-fs.writeFile(`${tmpdir}/nombre_del_archivo.txt`, 'contenido_del_archivo', function (err) {
-  if (err) throw err;
-  console.log('Archivo creado!');
-});
